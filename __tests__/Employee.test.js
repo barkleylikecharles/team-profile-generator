aca967f181e38720273b81cd2629dda98383a0eb
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-15 00:07:01 -0400

    passed first employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,13 @@
 const Employee = require("../lib/Employee");
 // Test - New employee creation
-test("Can create a new employee."), () => {
+test("Can create a new employee.", () => {
     const employee = new Employee ('Verona', 13, 'verona@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-    done();
-}
+    // done();
+});
 
 // test("Can add a name to employee object."), () => {
 //     const name = "Verona"
