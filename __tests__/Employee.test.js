40547ea7bea1060b16e01562cea7bf5ef23597c1
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-15 14:51:08 -0400

    getName testing

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,30 +8,31 @@ test("Can create a new employee.", () => {
     expect(employee.email).toEqual(expect.any(String));
     // done();
 });
-
+//Test - add name 
 test("Can add a name to employee object.", () => {
     const name = "Verona"
     const employee = new Employee(name);
     expect(employee.name).toBe(name);
 });
-
+// Test - add id
 test("Can add a ID# to employee object.", () => {
     const id = 13
     const employee = new Employee("Verona", 13);
     expect(employee.id).toBe(id);
 });
-
-test("Can add a email address to employee object.", () => {
+// Test - add email
+test("Can add an email address to employee object.", () => {
     const email = "Verona@gmail.com";
     const employee = new Employee("Verona", 13, email);
     expect(employee.email).toBe(email);
 });
 // Begin testing methods
-// test("Gets name through getName method.", () => {
-//     const testName = "Verona"
-//     const employee = new Employee("Verona");
-//     expect(employee.getName()).toBe(testName);
-// });
+// GetnName
+test("Gets name through getName method.", () => {
+    const testName = "Verona"
+    const employee = new Employee("Verona");
+    expect(employee.getName()).toBe(testName);
+});
 
 // test("Gets name through getId method.", () => {
 //     const testId = 13;
