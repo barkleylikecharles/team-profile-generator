94c9c9ebff0a18e0a5d179660e486c5d7760054f
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-14 23:47:37 -0400

    changes before bcs help

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,16 @@
 const Employee = require("../lib/Employee");
 // Test - New employee creation
-test("Can create a new employee."),
+test("Can create a new employee."), () => {
+    const employee = new Employee ('Verona', 13, 'verona@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    done();
+}
+
+// test("Can add a name to employee object."), () => {
+//     const name = "Verona"
+//     const employee = new Employee(name);
+//     expect(employee.name).toBe(name);
+// }
