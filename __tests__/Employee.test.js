250799604aa1bc7db778a7b5a5bee57e69c61464
Nicole Barkley <barkleylikecharles@icloud.com>
2022-04-15 09:45:00 -0400

    Employee object item testing

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,8 +9,20 @@ test("Can create a new employee.", () => {
     // done();
 });
 
-// test("Can add a name to employee object."), () => {
-//     const name = "Verona"
-//     const employee = new Employee(name);
-//     expect(employee.name).toBe(name);
-// }
+test("Can add a name to employee object.", () => {
+    const name = "Verona"
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+});
+
+test("Can add a ID# to employee object.", () => {
+    const id = 13
+    const employee = new Employee("Verona", 13);
+    expect(employee.id).toBe(id);
+});
+
+test("Can add a email address to employee object.", () => {
+    const email = "Verona@gmail.com";
+    const employee = new Employee("Verona", 13, email);
+    expect(employee.email).toBe(email);
+});
